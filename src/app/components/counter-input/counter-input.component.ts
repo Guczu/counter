8c9d5fc87856ad0a0c18cc5844ec099b90ed2427
Guczu <jakub180720@gmail.com>
2024-10-18 23:09:31 +0200

    add counter

---
+++ src/app/components/counter-input/counter-input.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-input',
+  templateUrl: './counter-input.component.html',
+  styleUrl: './counter-input.component.scss'
+})
+export class CounterInputComponent {
+  word: string = "";
+}
